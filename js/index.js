ea5a59345b8f9a85c91a85ce7209111510915750
Chrystian Martins <chrystianmsc@gmail.com>
2023-02-08 15:24:15 -0300

    save tab btn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabbtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 
@@ -27,18 +28,30 @@ function render(leads){
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
-
-
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
 
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
 
+    render(myLeads)
+})
+
+tabbtn.addEventListener("click", function () {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+   
+     })
+
+})
+
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
     render(myLeads)
 })
